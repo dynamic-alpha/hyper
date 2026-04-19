900e7bbc8a4dc7bfa8d2bebcfca4f6e7a019a444
Aleksei Sotnikov <asotnikov.100@gmail.com>
2026-04-19 14:34:20 +0700

    fix: eliminate action-not-found race condition with RwLock
    Problem: cleanup-tab-actions! created a gap where :actions was empty
    between cleanup and re-registration. In-flight HTTP action requests
    during this gap would fail with "Action not found".
    Solution: Use ReentrantReadWriteLock (FIFO) per tab to serialize
    cleanup+render (write lock) with action execution (read lock).

--- src/hyper/actions.clj
+++ src/hyper/actions.clj
@@ -35,20 +35,26 @@
 (defn execute-action!
   "Execute an action by ID with error handling.
    When client-params are provided they are passed to the action fn."
-  ([app-state* action-id]
-   (execute-action! app-state* action-id nil))
-  ([app-state* action-id client-params]
-   (if-let [action-data (get-in @app-state* [:actions action-id])]
-     (let [{:keys [fn]} action-data]
-       (t/catch->error! :hyper.error/execute-action
-                        (fn client-params))
-       true)
-     (do
-       (t/log! {:level :warn
-                :id    :hyper.error/action-not-found
-                :data  {:hyper/action-id action-id}
-                :msg   "Action not found"})
-       (throw (ex-info "Action not found" {:hyper/action-id action-id}))))))
+  ([app-state* tab-id action-id]
+   (execute-action! app-state* tab-id  action-id nil))
+  ([app-state* tab-id action-id client-params]
+   (let [tab-read-lock (.readLock (get-in @app-state* [:tabs tab-id :renderer :rw-lock]))]
+     (.lock tab-read-lock)
+     (try
+       (if-let [action-data (get-in @app-state* [:actions action-id])]
+         (let [{:keys [fn]} action-data]
+           (t/catch->error! :hyper.error/execute-action
+                            (fn client-params))
+           true)
+         (do
+           (t/log! {:level :warn
+                    :id    :hyper.error/action-not-found
+                    :data  {:hyper/action-id action-id}
+                    :msg   "Action not found"})
+           (throw (ex-info "Action not found" {:hyper/action-id  action-id
+                                               :hyper/action-ids (keys (get-in @app-state* [:actions]))}))))
+       (finally
+         (.unlock tab-read-lock))))))
 
 (defn cleanup-tab-actions!
   "Remove all actions for a tab."
